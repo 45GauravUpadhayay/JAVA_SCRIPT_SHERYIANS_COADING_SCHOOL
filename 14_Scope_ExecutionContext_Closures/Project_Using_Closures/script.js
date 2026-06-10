98a2b107da61471d9c880c0c08bc33d41f610ea5
45GauravUpadhayay <45upadhayaygaurav264@gmail.com>
2026-06-10 19:25:19 +0530

    2 nd issu fix

--- 14_Scope_ExecutionContext_Closures/Project_Using_Closures/script.js
+++ 14_Scope_ExecutionContext_Closures/Project_Using_Closures/script.js
@@ -2,13 +2,12 @@ function createToaster (config) {
     return function(str) {
         let div = document.createElement("div");
         div.textContent = str;
-        div.className = `inline-block ${config.theme === "dark" ? "bg-gray-800 text-white" : "bg-gray-100 text-black"} px-6 py-3 rounded shadow-lg pointer-eventes-none`;
+        div.className = `inline-block ${config.theme === "dark" ? "bg-gray-800 text-white" : "bg-gray-100 text-black"} px-6 py-3 rounded shadow-lg pointer-events-none`;
 
         document.querySelector(".parent").appendChild(div);
 
-        if(config.positionX !== "left" || positionY !== "top"){
-            document.querySelector(".parent").className += 
-            `${config.positionX === "right" ? "right-5" : " left-5"} ${config.positionY === "bottom" ? "bottom-5" : " top-5"}`;
+        if(config.positionX !== "left" || config.positionY !== "top"){
+            document.querySelector(".parent").className += `${config.positionX === "right" ? "right-5" : "left-5"} ${config.positionY === "bottom" ? "bottom-5" : "top-5"}`
         }
 
         setTimeout(() => {
@@ -18,9 +17,9 @@ function createToaster (config) {
 };
 
 let toaster = createToaster({
-    positionX: "right",
-    positionY: "bottom",
-    theme: "light",
+    positionX: "left",
+    positionY: "top",
+    theme: "dark",
     duration: 3,
 });
 
