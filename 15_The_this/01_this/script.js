150501118387b673dd2d70eaf042e25f0cb3cc48
45GauravUpadhayay <45upadhayaygaurav264@gmail.com>
2026-06-10 19:26:19 +0530

    1 st stage

---
+++ 15_The_this/01_this/script.js
@@ -0,0 +1,13 @@
+//* The this Keyword Special Keyword 
+
+//? this -> special special word
+
+//* this keyword ek special keyword hai, kyuki jaise ki bake sare keyword ki value ya unka nature same rahta hai this ki value ya nature badal jata hai is bat se ki aap use kaha use kar rahe ho
+
+//? global scope me this ki value -> window
+
+//* window hai Prime Minister
+
+//? 
+
+console.log(this);
