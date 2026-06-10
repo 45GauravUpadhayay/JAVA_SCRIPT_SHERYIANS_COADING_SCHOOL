0a4961a57a3e8857720afe2c4b47d02888006c20
45GauravUpadhayay <45upadhayaygaurav264@gmail.com>
2026-06-10 16:27:55 +0530

    1 st issu fixed

--- 14_Scope_ExecutionContext_Closures/Project_Using_Closures/script.js
+++ 14_Scope_ExecutionContext_Closures/Project_Using_Closures/script.js
@@ -1,25 +1,30 @@
-function createToaster(config) {
-    return function(nortification){
+function createToaster (config) {
+    return function(str) {
         let div = document.createElement("div");
-        div.className = `inline-block${config.theme === "dark" ? "bg-gray-800 text-white" : "bg-gray-100 text-black" } px-6 py-3 rounded shadow-lg pointer-events-none ${config.positionX === "right" ? "right-10" : "left-10"} ${config.positionY === "top" ? "top-10" : "bottom-10"}`;
+        div.textContent = str;
+        div.className = `inline-block ${config.theme === "dark" ? "bg-gray-800 text-white" : "bg-gray-100 text-black"} px-6 py-3 rounded shadow-lg pointer-eventes-none`;
 
-        div.textContent = nortification;
         document.querySelector(".parent").appendChild(div);
+
+        if(config.positionX !== "left" || positionY !== "top"){
+            document.querySelector(".parent").className += 
+            `${config.positionX === "right" ? "right-5" : " left-5"} ${config.positionY === "bottom" ? "bottom-5" : " top-5"}`;
+        }
 
         setTimeout(() => {
             document.querySelector(".parent").removeChild(div);
-        },config.duration * 1000)
-    }
-}
+        }, config.duration * 1000);
+    };
+};
 
 let toaster = createToaster({
     positionX: "right",
-    positionY: "top",
+    positionY: "bottom",
     theme: "light",
     duration: 3,
 });
 
-toaster("Download Done");
+toaster('Download Done');
 setTimeout(() => {
     toaster("Gaurav accepted your request")
 },2000);
