24973de3d9d9de06549d9a039d03821896e4c3b5
45GauravUpadhayay <45upadhayaygaurav264@gmail.com>
2026-06-12 08:46:11 +0530

    sync and async complete

---
+++ 17_callback_promises_Async_Await/Synchronous_asynchronous/script.js
@@ -0,0 +1,15 @@
+//* kai bar code js me line by line chalega aur yah natural pattern bhi hota hai ki code line by line chale but kabhi kabar aisa hota hai kai aise cases aate bhai life me ki jaha par aapka code wait karta hai and utni der me agla code chal jata hai
+
+console.log("hey1");
+console.log("hey2");
+setTimeout(() => {
+    console.log("hey3")
+}, 2000);
+console.log("hey4");
+
+//* SynchronousVs_asynchronous_JS
+
+//? aisa code jo line by line chale hota hai sync code
+
+//? aisa code jo jab chalne ke liye ho jaye tab chale vah hota hao async code
+
